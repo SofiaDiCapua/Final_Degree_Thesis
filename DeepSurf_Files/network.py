c5785fd2dbb63afaf6596eb66bf6ad5fa51053e1
Sofia Di Capua <73697639+SofiaDiCapua@users.noreply.github.com>
2024-06-15 20:47:58 +0200

    Update network.py

--- DeepSurf_Files/network.py
+++ DeepSurf_Files/network.py
@@ -10,6 +10,7 @@ import numpy as np, os
 import tensorflow as tf
 from tensorflow.contrib import slim
 from features import KalasantyFeaturizer
+from PUResNet_files.PUResNet import PUResNet
 
 
 class Network:
@@ -18,35 +19,50 @@ class Network:
         tf.reset_default_graph()
         self.inputs = tf.placeholder(tf.float32,shape=(None,gridSize,gridSize,gridSize,18))
         
-        if model=='orig':
-            from net.resnet_3d import resnet_arg_scope, resnet_v1_18
-        elif model=='lds':
-            from net.resnet_lds_3d_bottleneck import resnet_arg_scope, resnet_v1_18
-        
-        with slim.arg_scope(resnet_arg_scope()):  
-            self.net, self.end_points = resnet_v1_18(self.inputs, 1, is_training=False)
-        
+        # New implementation
+        if model == 'PUResNet':
+            self.model = PUResNet()
+        # Old implementation
+        else :
+            if model=='orig':
+                from net.resnet_3d import resnet_arg_scope, resnet_v1_18
+            elif model=='lds':
+                from net.resnet_lds_3d_bottleneck import resnet_arg_scope, resnet_v1_18
+            
+            with slim.arg_scope(resnet_arg_scope()):  
+                self.net, self.end_points = resnet_v1_18(self.inputs, 1, is_training=False)
+            
+
         config = tf.ConfigProto()
         config.gpu_options.allow_growth = True
         self.sess = tf.Session(config=config)
         
         self.sess.run(tf.global_variables_initializer()) 
-        saver = tf.train.Saver()
-        if model=='orig':
-            saver.restore(self.sess,os.path.join(model_path,'resnet18'))
-        elif model=='lds':
-            saver.restore(self.sess,os.path.join(model_path,'bot_lds_resnet18'))
+
         
-        self.featurizer = KalasantyFeaturizer(gridSize,voxelSize)
+        # New implementation that uses keras (as PUResNet does)
+        if model == 'PUResNet':
+            self.model.load_weights(os.path.join(model_path, 'final_weights.h5'))
+        # Old implementation that used tensorflow 
+        else :
+            saver = tf.train.Saver()
+            if model=='orig':
+                saver.restore(self.sess,os.path.join(model_path,'resnet18'))
+            elif model=='lds':
+                saver.restore(self.sess,os.path.join(model_path,'bot_lds_resnet18'))
+        
+        self.featurizer = KalasantyFeaturizer(gridSize,voxelSize) 
         
     def get_lig_scores(self, prot, batch_size):
-        self.featurizer.get_channels(prot.mol)
+        # First get the features
+        self.featurizer.get_channels(prot.mol) 
 
         gridSize = 16
         lig_scores = []
         input_data = np.zeros((batch_size,gridSize,gridSize,gridSize,18))  
         batch_cnt = 0
         for p,n in zip(prot.surf_points,prot.surf_normals):
+            # Then make the grids
             input_data[batch_cnt,:,:,:,:] = self.featurizer.grid_feats(p,n,prot.heavy_atom_coords)  
             batch_cnt += 1
             if batch_cnt==batch_size:
